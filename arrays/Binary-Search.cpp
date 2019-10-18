595e12d7557ea97fda77f358db7e1a6eaba4586a
gunavardhan963 <35570206+gunavardhan963@users.noreply.github.com>
2019-10-18 22:00:13 +0530

    commited

---
+++ arrays/Binary-Search.cpp
@@ -0,0 +1,12 @@
+int bin_search(int A[], int left, int right, int k)
+{
+    if(right>=left){
+        int mid = left + (right-left)/2;
+        if(A[mid] == k)
+            return mid;
+        if(A[mid]>k)
+            return bin_search(A, left, mid-1, k);
+        return bin_search(A, mid+1, right, k);
+    }
+    return -1;
+}
